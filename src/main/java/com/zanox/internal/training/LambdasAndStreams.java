ed59d97c2287e03d20297d6887379d5ac4ef4b69
FuBar <till@wohaltz.eu>
2018-12-06 10:06:20 +0100

    Java best praxis addition

---
+++ src/main/java/com/zanox/internal/training/LambdasAndStreams.java
@@ -0,0 +1,66 @@
+package com.zanox.internal.training;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+
+/**
+ * Training resources for lambdas, streams and method references
+ */
+public class LambdasAndStreams {
+
+
+    /**
+     * Dummy main for run within IDE
+     * @param args runtime args for the execution (leave empty, necessary standard of the main signature)
+     */
+    public static void main(String[] args) {
+
+        var myList=
+        IntStream.range(1, 10)
+                .filter(x -> x % 2 == 0) // Predicate implementation 
+                .mapToDouble(x -> x * 2) // Function implementation
+                .boxed()
+                .collect(Collectors.toList()); // Default Supplier usage
+
+        myList.forEach(System.out::println);// Consumer implementation and method reference
+        //(x -> System.out.print(String.format("Value : %s\r\n", x)));
+
+        // Custom implementation of custom interface
+        DummyInterface handler = (x)->{
+            var result=x
+                    .chars()
+                    .mapToObj(i -> (char) i)
+                    .map(c -> Character.toString(c))
+                    .reduce("", (s, c) -> s + c + c);
+            return result;
+        };
+        dummyFunction("Fubar", handler);
+
+        
+        
+    }
+
+    /**
+     * Dummy function for functional {@link DummyInterface} interface as parameter
+     * @param with String value to transform
+     * @param handler Transformation function
+     */
+    public static void dummyFunction(String with, DummyInterface handler) {
+        System.out.print(
+                handler.handleTheDummy(with)
+        );
+    }
+
+    /**
+     * Custom functional interface
+     */
+    @FunctionalInterface
+    interface DummyInterface {
+        public String handleTheDummy(String input);
+    }
+    
+}
